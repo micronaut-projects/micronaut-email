ad8ddde039d13f69e3ff5fb9dad0b38ca2f7f9b0
Sergio del Amo <sergio.delamo@softamo.com>
2023-04-26 11:34:54 +0200

    M2 (#212)
    * M2
    
    * annotate test with @PendingFeature

--- email/src/test/groovy/io/micronaut/email/TransactionalEmailSenderSpec.groovy
+++ email/src/test/groovy/io/micronaut/email/TransactionalEmailSenderSpec.groovy
@@ -9,6 +9,8 @@ import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
 import jakarta.inject.Named
 import jakarta.inject.Singleton
+import spock.lang.Issue
+import spock.lang.PendingFeature
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -27,6 +29,21 @@ class TransactionalEmailSenderSpec extends Specification {
     @Inject
     BeanContext beanContext
 
+    @Issue("https://github.com/micronaut-projects/micronaut-validation/issues/101")
+    @PendingFeature
+    void "default message of AnyRecipient annotation is used"(List<String> messages, Email email, String description) {
+        when: 'without a from address'
+        emailSender.send(email)
+
+        then: 'a validation exception is thrown'
+        ConstraintViolationException e = thrown()
+        messages == e.constraintViolations*.message
+
+        where:
+        messages                               | email                                                                                              | description
+        [EmailMessages.ANY_RECIPIENT_MESSAGE]  | Email.builder().from("tcook@apple.com").subject("Hello World").body("I love Apple Music").build()  | 'any recipient is validated'
+    }
+
     @Unroll("#description")
     void "Email is validated before is sent"(List<String> messages, Email email, String description) {
         when: 'without a from address'
@@ -38,7 +55,6 @@ class TransactionalEmailSenderSpec extends Specification {
 
         where:
         messages                               | email                                                                                              | description
-        [EmailMessages.ANY_RECIPIENT_MESSAGE]  | Email.builder().from("tcook@apple.com").subject("Hello World").body("I love Apple Music").build()  | 'any recipient is validated'
         ["must not be blank"]                  | Email.builder().from("tcook@apple.com").to("ecue@apple.com").body("I love Apple Music").build()    | 'subject is validated'
         ["must not be null"]                   | Email.builder().subject("Hello World").to("ecue@apple.com").body("I love Apple Music").build()     | 'from is validated'
         ["must not be null"]                   | Email.builder().from("tcook@apple.com").to("ecue@apple.com").subject("Hello World").build()        | 'body is required'
