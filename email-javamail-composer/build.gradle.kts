1522cf019cfe072e1a2ae5640fe1a97bc110f8f1
Dean Wette <wetted@unityfoundation.io>
2023-06-07 10:16:38 -0500

    fix missing dependency

--- email-javamail-composer/build.gradle.kts
+++ email-javamail-composer/build.gradle.kts
@@ -7,6 +7,8 @@ dependencies {
     api(projects.micronautEmail)
     api(libs.managed.jakarta.mail)
     implementation(mnValidation.micronaut.validation)
+    runtimeOnly(libs.managed.eclipse.angus)
+
     testImplementation(projects.testSuiteUtils)
     testImplementation(mn.micronaut.http)
     testCompileOnly(mn.micronaut.inject.groovy)
