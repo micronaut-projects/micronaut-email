a19d06b795bb79c88bc873b5736eb13c003a91e1
Dean Wette <wetted@unityfoundation.io>
2023-06-08 08:57:19 -0500

    Document additional runtime dependency requirement for Jakarta Mail.

--- email-javamail-composer/build.gradle.kts
+++ email-javamail-composer/build.gradle.kts
@@ -7,9 +7,9 @@ dependencies {
     api(projects.micronautEmail)
     api(libs.managed.jakarta.mail)
     implementation(mnValidation.micronaut.validation)
-    runtimeOnly(libs.managed.eclipse.angus)
 
     testImplementation(projects.testSuiteUtils)
     testImplementation(mn.micronaut.http)
     testCompileOnly(mn.micronaut.inject.groovy)
+    testRuntimeOnly(libs.managed.eclipse.angus)
 }
