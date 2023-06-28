45e1d883fed7dca16d636189bd672cad73871be9
Milenko Supic <supic82@gmail.com>
2023-06-28 10:42:56 +0200

    Added native tests and removed unnecessary metadata

--- test-suite-graal/src/test/java/example/micronaut/SesEmailComposerTest.java
+++ test-suite-graal/src/test/java/example/micronaut/SesEmailComposerTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @MicronautTest(startApplication = false)
-public class SesEmailComposerServiceTest {
+public class SesEmailComposerTest {
 
     @Inject
     SesEmailComposer composer;
