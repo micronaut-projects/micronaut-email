cb08214bbfbe16fbc98d9bbd0edb2f8787a653da
Guillermo Calvo <calvog@objectcomputing.com>
2023-07-27 11:24:05 +0200

    Populate Postmark fields: cc/bcc/replyTo

--- email-postmark/src/main/java/io/micronaut/email/postmark/PostmarkEmailComposer.java
+++ email-postmark/src/main/java/io/micronaut/email/postmark/PostmarkEmailComposer.java
@@ -30,7 +30,6 @@ import jakarta.inject.Singleton;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * Composes a {@link Message} given an {@link io.micronaut.email.Email}.
@@ -59,7 +58,16 @@ public class PostmarkEmailComposer implements EmailComposer<Message> {
             message.setFrom(email.getFrom().getEmail());
         }
         if (email.getTo() != null) {
-            message.setTo(email.getTo().stream().map(Contact::getEmail).collect(Collectors.toList()));
+            message.setTo(email.getTo().stream().map(Contact::getEmail).toList());
+        }
+        if (email.getCc() != null) {
+            message.setCc(email.getCc().stream().map(Contact::getEmail).toList());
+        }
+        if (email.getBcc() != null) {
+            message.setBcc(email.getBcc().stream().map(Contact::getEmail).toList());
+        }
+        if (email.getReplyTo() != null) {
+            message.setReplyTo(email.getReplyTo().getEmail());
         }
         message.setSubject(email.getSubject());
         Body body = email.getBody();
