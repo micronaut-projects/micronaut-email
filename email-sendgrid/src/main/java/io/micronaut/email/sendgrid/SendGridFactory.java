cc5e6020cf9abdd3c30511dc8f43894761581da4
Sergio del Amo <sergio.delamo@softamo.com>
2025-11-27 10:36:46 +0100

    remove extra blank line

--- email-sendgrid/src/main/java/io/micronaut/email/sendgrid/SendGridFactory.java
+++ email-sendgrid/src/main/java/io/micronaut/email/sendgrid/SendGridFactory.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.micronaut.email.sendgrid;
 
 import com.sendgrid.SendGrid;
