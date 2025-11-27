bc567b79a41e8e2fe3a30ab167f0759e805a6be8
Sergio del Amo <sergio.delamo@softamo.com>
2025-11-27 10:44:22 +0100

    doc: SendGridBeanCreatedEventListener

--- email-sendgrid/src/test/groovy/io/micronaut/email/sendgrid/SendGridSpec.groovy
+++ email-sendgrid/src/test/groovy/io/micronaut/email/sendgrid/SendGridSpec.groovy
@@ -3,10 +3,17 @@ package io.micronaut.email.sendgrid
 import com.sendgrid.SendGrid
 import io.micronaut.context.BeanContext
 import io.micronaut.context.annotation.Property
+import io.micronaut.context.annotation.Requires
+import io.micronaut.context.event.BeanCreatedEvent
+import io.micronaut.context.event.BeanCreatedEventListener
+import io.micronaut.core.annotation.NonNull
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Singleton
 import spock.lang.Specification
 import jakarta.inject.Inject
 
+
+@Property(name = "spec.name", value = "SendGridSpec")
 @Property(name = "sendgrid.api-key", value = "xxx")
 @MicronautTest(startApplication = false)
 class SendGridSpec extends Specification {
@@ -17,5 +24,12 @@ class SendGridSpec extends Specification {
     void "if you set sendgrid.enabled to false you disable Sendgrid integration"() {
         expect:
         beanContext.containsBean(SendGrid)
+
+        when:
+        SendGrid sendGrid = beanContext.getBean(SendGrid)
+
+        then:
+        sendGrid
+        5000 == sendGrid.getRateLimitSleep()
     }
 }
