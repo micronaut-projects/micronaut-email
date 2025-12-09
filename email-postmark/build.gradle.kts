ddfbd4c2b001e01cf3ede108aa76d53ba646e53d
Sergio del Amo <sergio.delamo@softamo.com>
2025-12-09 09:56:31 +0100

    oss index audit

--- email-postmark/build.gradle.kts
+++ email-postmark/build.gradle.kts
@@ -4,7 +4,10 @@ plugins {
 
 dependencies {
     api(libs.managed.postmark)
-    implementation(libs.commons.io) // postmark contains a vulnerable version of commons-io
+    // postmark depends on com.fasterxml.jackson.core/jackson-core@2.14.1 which contains a 1 vulnerability
+    // https://ossindex.sonatype.org/vulnerability/CVE-2025-52999?component-type=maven&component-name=com.fasterxml.jackson.core%2Fjackson-core
+    implementation("com.fasterxml.jackson.core:jackson-core:2.15.0") // postmark depends on jackson-core  version
+
     api(projects.micronautEmail)
     implementation(mn.reactor)
     testImplementation(mn.micronaut.http)
